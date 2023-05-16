62ba22c4fe2988b43a44e0662906e6d37062a279
j-carlander <johannes.carlander@gmail.com>
2023-05-16 11:07:15 +0200

    added symbol for when more than one image in gallery

--- src/Components/GalleryCard/GalleryCard.jsx
+++ src/Components/GalleryCard/GalleryCard.jsx
@@ -3,6 +3,7 @@ import "./GalleryCard.css";
 export function GalleryCard({ preview, altText, images, handleImgClick }) {
   return (
     <figure className="gallery-card">
+      {images.length > 1 && <p className="gallery-multi-sym">&#10064;</p>}
       <img
         className="gallery-card-img"
         src={preview}
